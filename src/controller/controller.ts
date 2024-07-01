8bdf9a23723291be4f76c15a9c8c6092ffe61130
ogedi001 <ogedifavour2@gmail.com>
2024-07-01 21:48:13 +0100

    minor fix

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -8,16 +8,24 @@ export const getClientWeatherGreeting = async (req: Request, res: Response, next
     if (!visitor_name)
         throw new BadRequestError(' "Visitor_name" is a required field in the query parameters.');
 
-    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    if (!clientIp) throw new BadRequestError('Could not determine client IP address.');
+    const clientIp = req.headers['x-forwarded-for'] || req.headers['x-real-ip'] || req.connection.remoteAddress;
 
-    const locationInfo = await getUserLocationInfo(clientIp, next)
+  // Check if clientIp is an array (it can be in some cases)
+  const ip = Array.isArray(clientIp) ? clientIp[0] : clientIp;
+
+  // Log the client's IP address (for demonstration purposes)
+  console.log('Client IP Address:', ip);
+
+  if (!ip) {
+    throw new BadRequestError('Could not determine client IP address.');
+  }
+    const locationInfo = await getUserLocationInfo(ip, next)
 
     const weather = await getUserWeather(locationInfo.latitude, locationInfo.longitude, next)
     const temperature = weather.main.temp;
     const responseJson = {
         client_ip: clientIp,
-        location: weather.name,
+        location: locationInfo.city,
         greeting: `Hello, ${visitor_name}! The temperature is ${temperature.toFixed(1)} degrees Celsius in ${weather.name}`
     };
 
